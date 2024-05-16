6e1afaea5e8a10d7215d7d15a8028a9900741d06
k-muell <keithmueller2000@gmail.com>
2024-05-16 17:56:20 -0500

    Early Code for adding songs

--- HeroKeyboardGuitar/HeroKeyboardGuitar/FrmMenu.Designer.cs
+++ HeroKeyboardGuitar/HeroKeyboardGuitar/FrmMenu.Designer.cs
@@ -33,35 +33,49 @@ namespace HeroKeyboardGuitar
         {
             ModeDropDown = new ComboBox();
             btnplay = new Button();
+            button1 = new Button();
             SuspendLayout();
             // 
             // ModeDropDown
             // 
             ModeDropDown.FormattingEnabled = true;
             ModeDropDown.Items.AddRange(new object[] { "Default", "Color Blind Mode" });
-            ModeDropDown.Location = new System.Drawing.Point(539, 235);
+            ModeDropDown.Location = new System.Drawing.Point(616, 313);
+            ModeDropDown.Margin = new Padding(3, 4, 3, 4);
             ModeDropDown.Name = "ModeDropDown";
-            ModeDropDown.Size = new System.Drawing.Size(200, 23);
+            ModeDropDown.Size = new System.Drawing.Size(228, 28);
             ModeDropDown.TabIndex = 1;
             // 
             // btnplay
             // 
-            btnplay.Location = new System.Drawing.Point(555, 364);
+            btnplay.Location = new System.Drawing.Point(634, 485);
+            btnplay.Margin = new Padding(3, 4, 3, 4);
             btnplay.Name = "btnplay";
-            btnplay.Size = new System.Drawing.Size(130, 32);
+            btnplay.Size = new System.Drawing.Size(149, 43);
             btnplay.TabIndex = 2;
             btnplay.Text = "Play";
             btnplay.UseVisualStyleBackColor = true;
             btnplay.Click += btnplay_Click;
             // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(172, 312);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // FrmMenu
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = System.Drawing.SystemColors.WindowFrame;
-            ClientSize = new System.Drawing.Size(800, 450);
+            ClientSize = new System.Drawing.Size(914, 600);
+            Controls.Add(button1);
             Controls.Add(btnplay);
             Controls.Add(ModeDropDown);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmMenu";
             Text = "Menu";
             WindowState = FormWindowState.Maximized;
@@ -75,5 +89,6 @@ namespace HeroKeyboardGuitar
 
         private System.Windows.Forms.ComboBox ModeDropDown;
         private Button btnplay;
+        private Button button1;
     }
 }
