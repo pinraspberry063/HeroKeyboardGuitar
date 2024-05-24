666a27be7d5a26ba9f3c708cf581b87b0ecf9728
k-muell <keithmueller2000@gmail.com>
2024-05-24 13:19:26 -0500

    Revert "Delete HeroKeyboardGuitar/HeroKeyboardGuitar/Code/Score.cs"
    This reverts commit 995676d17fccd57a7a61c6465dd01d15666bac51.

---
+++ HeroKeyboardGuitar/HeroKeyboardGuitar/Code/Score.cs
@@ -0,0 +1,57 @@
+﻿namespace HeroKeyboardGuitar;
+
+/// <summary>
+/// Holds the current score and streak of the player
+/// </summary>
+public class Score {
+    /// <summary>
+    /// Amount of notes successfully hit
+    /// </summary>
+    public int Amount { get; private set; }
+
+    /// <summary>
+    /// Current streak, i.e. consecutive hit notes without a miss
+    /// </summary>
+    public int Streak { get; private set; }
+
+    /// <summary>
+    /// initializes both amount and streak to 0
+    /// </summary>
+    public Score() {
+        Amount = 0;
+        Streak = 0;
+    }
+
+    /// <summary>
+    /// Used to check if the player has reached the point of receiving a reward.
+    /// Currently not implemented
+    /// </summary>
+    public void CheckReward() {
+        // TODO: possibly make this a dictionary mapping genres to reward maps
+        //Game.GetRewardMap()
+    }
+
+    /// <summary>
+    /// Add to the current score
+    /// </summary>
+    /// <param name="amount">Amount to add</param>
+    public void Add(int amount) {
+        if (Streak >= 5)
+        {
+            Amount += amount * 2;
+            Streak++;
+        }
+        else
+        {
+            Amount += amount;
+            Streak++;
+        }
+    }
+
+    /// <summary>
+    /// Resets streak back to 0
+    /// </summary>
+    public void Miss() {
+        Streak = 0;
+    }
+}
