ab90bef2b9da734be052503a8597acf76549d230
k-muell <keithmueller2000@gmail.com>
2024-05-24 14:49:43 -0500

    Adding more score multipliers and attempted to implement playing a sound after achieving a certain streak, but the sound isn't playing for some reason

--- HeroKeyboardGuitar/HeroKeyboardGuitar/Code/Score.cs
+++ HeroKeyboardGuitar/HeroKeyboardGuitar/Code/Score.cs
@@ -1,9 +1,21 @@
-﻿namespace HeroKeyboardGuitar;
+﻿
+using NAudio.Wave;
+using System;
+using System.Windows.Forms;
+namespace HeroKeyboardGuitar;
 
 /// <summary>
 /// Holds the current score and streak of the player
 /// </summary>
 public class Score {
+    private static string SOUND_ROOT_PATH = $"{Application.StartupPath}../../../Sounds/sound25/";
+    AudioFileReader sound25 = new AudioFileReader("C: \Users\keith\source\repos\HeroKeyboardGuitar\HeroKeyboardGuitar\HeroKeyboardGuitar\Sounds\sound25");
+    //AudioFileReader sound50 = new AudioFileReader(SOUND_ROOT_PATH + "sound50/");
+    //AudioFileReader sound100 = new AudioFileReader(SOUND_ROOT_PATH + "sound100/");
+    WaveOutEvent outputDevice25 = new WaveOutEvent();
+    //WaveOutEvent outputDevice50 = new WaveOutEvent();
+    //WaveOutEvent outputDevice100 = new WaveOutEvent();
+
     /// <summary>
     /// Amount of notes successfully hit
     /// </summary>
@@ -15,21 +27,17 @@ public class Score {
     public int Streak { get; private set; }
 
     /// <summary>
-    /// initializes both amount and streak to 0
+    /// initializes amount, streak, and sounds
     /// </summary>
     public Score() {
         Amount = 0;
         Streak = 0;
+        Console.WriteLine(SOUND_ROOT_PATH);
+        outputDevice25.Init(sound25);
+        //outputDevice50.Init(sound50);
+        //outputDevice100.Init(sound100);
     }
 
-    /// <summary>
-    /// Used to check if the player has reached the point of receiving a reward.
-    /// Currently not implemented
-    /// </summary>
-    public void CheckReward() {
-        // TODO: possibly make this a dictionary mapping genres to reward maps
-        //Game.GetRewardMap()
-    }
 
     /// <summary>
     /// Add to the current score
@@ -41,12 +49,38 @@ public class Score {
             Amount += amount * 2;
             Streak++;
         }
+        else if (Streak >= 10)
+        {
+            Amount += amount * 3;
+            Streak++;
+        }
+        else if (Streak >= 20)
+        {
+            Amount += amount * 4;
+            Streak++;
+        }
         else
         {
             Amount += amount;
             Streak++;
         }
+
+        switch (Streak)
+        {
+            case 3:
+                outputDevice25.Play();
+                break;
+            case 50:
+                //outputDevice50.Play();
+                break;
+            case 100:
+                //outputDevice100.Play();
+                break;
+            default:
+                break;
+        }
     }
+
 
     /// <summary>
     /// Resets streak back to 0
